3743de85ee5647264fc219fa424eaef77e23b524
dandemavani <dandemavani@gmail.com>
2023-01-06 18:27:32 +0530

    avani

---
+++ set_a_bit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int num,
+	printf("eneter the number \n");
+	scanf("%d",&num);
+	if(num&1)
+	{
+		printf("lsb bit is set %d\n",num);
+	}
+	else
+	{
+		printf("lsb bit is clear %d\n",num);
+	}
+}
